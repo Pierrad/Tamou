075172a8924a7fbab643248271bc4028de5e80ea
wassim <wassimbouzakri@icloud.com>
2022-02-17 23:20:09 +0100

    :construction: #3 Working on register page

---
+++ src/scenes/register/index.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import * as SC from './styled'
+import Input from '../../components/Input'
+import ActionButton from '../../components/ActionButton'
+import Link from '../../components/Link'
+
+
+
+const RegisterScreen = ({navigation}) => (
+	<SC.Container>
+		<SC.Head></SC.Head>
+		<SC.Contain1>
+			<SC.Contain2>
+				<SC.Title>Bienvenue</SC.Title>
+				<SC.Subtitle>Inscrivez vous pour continuer</SC.Subtitle>
+			</SC.Contain2>
+			<SC.Contain3>
+				<Input 
+					placeholder= "Username"
+					underlineColor="black">
+				</Input>
+				<Input 
+					placeholder= "Password" isPassword={true}
+					underlineColor="black">
+				</Input>
+				<Input 
+					placeholder= "Email" 
+					underlineColor="black">
+				</Input>
+				<Input 
+					placeholder= "Nom" 
+					underlineColor="black">
+				</Input>
+				<Input 
+					placeholder= "Prenom" 
+					underlineColor="black">
+				</Input>
+				<Input 
+					placeholder= "Date de naissance" 
+					underlineColor="black">
+				</Input>
+			</SC.Contain3>
+			<SC.Contain4>
+				<ActionButton  
+					primary={true} 
+					title="Inscription" 
+					onPress={()=> navigation.navigate('Home')}
+					underlayColor="orange"
+				/>
+			</SC.Contain4>
+			<SC.Contain5>
+				<SC.Text>Vous avez déjà un compte?</SC.Text>
+				<Link 
+					title = "Connexion" 
+					onPress={()=> navigation.navigate('Home')}
+					underlayColor="none"
+				/>
+			
+			</SC.Contain5>
+		</SC.Contain1>
+	</SC.Container>
+)
+
+RegisterScreen.propTypes = {
+	navigation: PropTypes.shape({
+		navigate: PropTypes.func.isRequired,
+	}).isRequired,
+}
+
+export default RegisterScreen
---
+++ src/scenes/register/styled.js
@@ -0,0 +1,78 @@
+import styled from 'styled-components/native'
+
+export const Container = styled.SafeAreaView`
+  flex:1;
+  background-color: ${props => props.theme.PRIMARY_BACKGROUND_COLOR};
+  background-color:#F99030;
+`
+export const Head = styled.View`
+padding: 70px;
+`
+export const Title = styled.Text`
+padding:30px;
+font-weight:bold;
+font-weight: 800;
+font-size: 24px;
+line-height: 30px;
+color: #000000;
+
+`
+
+export const Subtitle = styled.Text`
+margin-top:-50px;
+padding:30px;
+color:#898A8D;
+`
+
+
+export const Text = styled.Text`
+font-size: 16px;
+line-height: 20px;
+color: #898A8D;
+text-align:center;
+`
+export const Contain1 = styled.SafeAreaView`
+  flex: 1;
+  background-color: ${props => props.theme.PRIMARY_BACKGROUND_COLOR};
+  background:white;
+  border-top-left-radius: 30px;
+  border-top-right-radius: 30px;
+  
+  
+`
+
+export const Contain2 = styled.SafeAreaView`
+ 
+  flex:3;
+  background-color: ${props => props.theme.PRIMARY_BACKGROUND_COLOR};
+  background: transparent;
+  
+
+  
+`
+
+export const Contain3 = styled.SafeAreaView`
+margin-top: -70px;
+padding:40px;
+flex:9;
+background-color:transparent;
+
+
+`
+
+export const Contain4 = styled.SafeAreaView`
+flex:2;
+background-color:transparent;
+
+
+
+`
+
+export const Contain5 = styled.SafeAreaView`
+margin-top:-20px;
+flex:3;
+`
+
+
+
+
